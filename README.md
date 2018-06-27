a37137e56a21b13ccef7f4d65aced4e37faa543d
John <iziamos@gmail.com>
2018-06-27 23:13:46 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+
+#Factoral of 100!
+##What is this?
+Calculate the factoral of 100 and output it!
+##Why?
+The point is to do this in as few characters possible. Someone told me about this challenge so I gave it a shot!
+## Instructions
+no makefile... naturally :)
+just cc the file!
---
+++ factoral.c
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+#define	F for(j = 0; j < 158;) {
+
+main(){
+  int i,j,r[158] = {0};
+  *r = i = 1;
+
+  while(++i < 101) {
+    F
+      r[j++] *= i;
+    }
+    F
+      r[j+2] += r[j] / 100;
+      r[j] %= 100;
+      r[j+1] += r[j] / 10;
+      r[j++] %= 10;
+    }
+  }
+  F
+    printf("%d",r[157-j++]);
+  }
+}
